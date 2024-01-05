e4370df67ce7f595caadf601a9b92bc58ab049a9
evelina <91142355+135ze@users.noreply.github.com>
2024-01-05 15:01:59 -0500

    week 2

--- medium/0053_maximum_subarray.py
+++ medium/0053_maximum_subarray.py
@@ -1,24 +1,29 @@
 from typing import List
+# 53. Maximum Subarray
+# Array, Divide and Conquer, DP
+
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+# -----------------------------------------------
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # calculate the sum of subarrays from 0 to each num
-        acc_sum = [0]
+        cur_sum, max_sum = 0, nums[0]
         for num in nums:
-            acc_sum.append(num + acc_sum[-1])
-        acc_sum = acc_sum[1:] # discard dummy 0 from init
-
-        max_acc = acc_sum[0] 
-        max_sum = nums[0]
-        print(acc_sum)
-        for sum in acc_sum:
-            print (sum, max_acc, max_sum)
-            if sum > max_acc:
-                max_sum = max(sum - max_acc, max_sum)
-                max_acc = sum
-
+            # max sum from subarray with previous vs num
+            cur_sum = max(num, cur_sum + num)
+            # compare w previous
+            max_sum = max(max_sum, cur_sum)
         return max_sum
 
+        # divide and conquer from sols
+        pre, suf = [*nums], [*nums]
+        for i in range(1, len(nums)):       pre[i] += max(0, pre[i-1])
+        for i in range(len(nums)-2,-1,-1):  suf[i] += max(0, suf[i+1])
+        def maxSubArray(A, L, R):
+            if L == R: return A[L]
+            mid = (L + R) // 2
+            return max(maxSubArray(A, L, mid), maxSubArray(A, mid+1, R), pre[mid] + suf[mid+1])
+        return maxSubArray(nums, 0, len(nums)-1)
 # -----------------------------------------------
 # Testing
 my_sol = Solution()
